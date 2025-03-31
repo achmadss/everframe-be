1fee544afdd8e61fe52de4c7fb3603d4bf70b32c
achmadss <anakinskywalk1@gmail.com>
2025-04-01 02:11:08 +0700

    refactor: change download directory to get method

--- src/main/kotlin/file_upload/controller/FileDownloadController.kt
+++ src/main/kotlin/file_upload/controller/FileDownloadController.kt
@@ -2,7 +2,6 @@ package dev.achmad.file_upload.controller
 
 import io.ktor.server.application.Application
 import io.ktor.server.routing.get
-import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 
@@ -12,7 +11,7 @@ object FileDownloadController {
         with(application) {
             routing {
                 route("/api/v1/downloads") {
-                    post("/{directory}") {
+                    get("/{directory}") {
 
                     }
                 }
